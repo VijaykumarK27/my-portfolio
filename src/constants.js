9f335f3fc23c37564105f6ceb12abc63e76689b3
VijaykumarK27 <vijaykumar.kadappagol2001@gmail.com>
2024-01-03 22:44:36 +0530

    Changes

--- src/constants.js
+++ src/constants.js
@@ -46,8 +46,11 @@ export const techStack = [
     name: "VS Code",
     link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vscode/vscode-original.svg",
   },
-  { name: "Sendinblue", link: "https://assets.stickpng.com/images/62e14f6feb4d9a9dc054c195.png" },
+  { name: "Sendinblue",
+  link: "https://assets.stickpng.com/images/62e14f6feb4d9a9dc054c195.png" },
+
 ];
+
 
 export const contactLinks = [
   {
@@ -82,11 +85,32 @@ export const serviceData = [
 
 export const projects = [
   {
-    link: "",
-    img: "",
-    desc: "",
-    source: "",
-  },  
+    link: "https://cozyfox-umber.vercel.app/",
+    img: "https://firebasestorage.googleapis.com/v0/b/image-link-7294d.appspot.com/o/vig%2F104b4d95-6f2a-46d0-ad1c-3eb3bd6c12e6_web_png.png?alt=media&token=eef65983-7745-4679-9f85-9db3465ad525",
+    desc: "Login Page",
+    source: "https://github.com/VijaykumarK27/CozyFox-Website.git",
+  },
+  {
+    link: "https://digital-clock-git-main-vijaykumark27.vercel.app/",
+    img: "https://firebasestorage.googleapis.com/v0/b/image-link-7294d.appspot.com/o/vig%2F7019f439-5421-4f81-b0f2-561e1e7ded81_dg.png?alt=media&token=053500f9-d9bf-4dd7-b463-d711174eb22b",
+    desc: "Digital Clock",
+    source: "https://github.com/VijaykumarK27/DigitalClock.git",
+    },
+
+    { 
+    link: "https://calculator-two-umber.vercel.app/",
+    img: "https://firebasestorage.googleapis.com/v0/b/image-link-7294d.appspot.com/o/vig%2Fa6576788-f555-4ad7-b9e5-a743b5ee1c08_c.png?alt=media&token=77f69f4c-4c12-40f0-a143-1b9d3c89d722",
+    desc: "Calculator",
+    source: "https://github.com/VijaykumarK27/Calculator.git",
+    },
+
+    {
+    link: "https://bmi-calculator-tdzj.vercel.app/",
+    img: "https://firebasestorage.googleapis.com/v0/b/image-link-7294d.appspot.com/o/vig%2F95b44da5-d33e-473a-97f2-e4dfb8ad2622_bc.png?alt=media&token=8330fa4c-33dd-4ec2-91ef-9e3272814207",
+    desc: "BMI-Calculator",
+    source: "https://github.com/VijaykumarK27/BMI-CALCULATOR.git",
+    },
+    
 ];
 
 export const personalInfo = [
@@ -105,11 +129,12 @@ export const personalInfo = [
 ];
 
 export const aboutMe =  {
-  fullName: 'Vijaykumar Kadappagol',
+  fullName: 'VIJAYKUMAR KADAPPAGOL',
   name: 'Vijaykumar',
-  description:'',
+  description:'I am Intern Graduate',
   resumeLink:'https://drive.google.com/file/d/1LbeEch0_5SO8uVTiZbk6CPbmoyvWgbqx/view?usp=drive_link',
-  aBitAboutMe:'',
-  technologiesAndTools:'',
+  aBitAboutMe:'Final Year Student-2023, Bachelor of Engineering in Computer Science, Presidency University. Enthusiastic and driven computer science graduate seeking an entry-level ,position to use my technical ability and develop my professional skills.I am a Full Stack Developer with proficiency in front-end technologies (HTML,CSS, JavaScript) and back-end skills (Java, Node.js).Knowledge in Software Development Life Cycle and Software Testing Life Cycle. Experienced in database management (SQL) and API development. I excel increating dynamic, user-friendly web solutions, deploying with Git and Docker.Staying current with industry trends for innovative project outcomes.',
+  technologiesAndTools:'In web development, crucial technologies and tools encompass a broad spectrum, including front-end, back-end, and infrastructure elements. ',
+  
 }
 
